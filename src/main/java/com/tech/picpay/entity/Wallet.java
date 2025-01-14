4d46a0680b67b51408427f9cba4d3afc9762948f
alexandre <alexandrec658@gmail.com>
2025-01-14 18:50:06 -0300

    mapeamento das entidades 'wallet', 'wallet_type' e config no application.yml

---
+++ src/main/java/com/tech/picpay/entity/Wallet.java
@@ -0,0 +1,43 @@
+package com.tech.picpay.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_wallet")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Wallet{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "cnpj_cpf", unique = true)
+    private String cnpjCpf;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "balance")
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_type_id")
+    private WalletType walletType;
+
+
+
+}
---
+++ src/main/java/com/tech/picpay/entity/WalletType.java
@@ -0,0 +1,21 @@
+package com.tech.picpay.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_wallet_type")
+@NoArgsConstructor
+@Getter
+@Setter
+public class WalletType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+}
